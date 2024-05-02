f932662a78d15a58a7022e135e2445cf43ca2299
Maikon Henrique <maikonhenrique189@gmail.com>
2024-05-02 00:41:55 -0300

    feat(header); adicionar menu responsivo

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/*Função para abrir o menu*/
+function openMenu(){
+    document.getElementById("menu_mobile").style.width = "50%";
+}
+
+/*Função para fechar o menu*/
+function closeMenu(){
+    document.getElementById("menu_mobile").style.width = "0%";
+}
